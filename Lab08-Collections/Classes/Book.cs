1be793def2c0f461de60567e1394883a8cf454fa
Mike Falk <falk478@yahoo.com>
2021-01-20 22:19:18 -0800

    First pass at lab 08 will need to come back and revisit it again

---
+++ Lab08-Collections/Classes/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08_Collections
+{
+   class Book<T>
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public GenreType Genre { get; set; }
+
+
+    }
+
+    enum GenreType
+    {
+        Action,
+        Classics,
+        Comic,
+        Mystery,
+        Fantasy,
+        Historical_Fiction,
+        Horror,
+        Literary_Fiction,
+        Magical_Realism,
+        Romance,
+        Sci_Fi,
+        Short_Stories,
+        Suspense,
+        Non_Fiction,
+        CookBooks,
+        History,
+        True_Crime,
+        Educational
+    }
+}
---
+++ Lab08UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lab08UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
